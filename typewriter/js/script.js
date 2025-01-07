0728f2bf73cc88a597b50fe7b01f7b6b2828f8f7
pixelnotperfect <yuliia.devweb@gmail.com>
2025-01-07 20:45:25 +0200

    Add files via upload

---
+++ typewriter/js/script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+window.addEventListener('load', () => {
+	const element = document.querySelector('.typewriter');
+
+	if (element) { 
+		element.classList.add('start-typing');
+	}
+});
